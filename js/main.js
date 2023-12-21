de2c1e82d78bc0df2d9be0d0bf226a1d0427f4c3
mario12magdy <mario2012magdy@hotmail.com>
2023-12-21 10:09:54 +0200

    New

--- js/main.js
+++ js/main.js
@@ -1,43 +1,42 @@
 var nemeInput = document.getElementById("siteName")
 var urlInput = document.getElementById("siteURL")
 
-var productList =[];
-if(localStorage.getItem("productData")!= null){
-productList = JSON.parse(localStorage.getItem("productData"))
+var siteList =[];
+if(localStorage.getItem("siteList")!= null){
+siteList = JSON.parse(localStorage.getItem("siteList"))
 display();
 }
-function addProduct(){
-    var product = {
+function addSite(){
+    var site = {
         name:nemeInput.value,
         url:urlInput.value,
        
     }
-    productList.push(product)
-    localStorage.setItem("productData", JSON.stringify(productList));
+    siteList.push(site)
+    localStorage.setItem("siteList", JSON.stringify(siteList));
     display();
 
 }
 function visit(x){
     location.href=x[urlInput.value];
 }
-function deleteProduct(x){
-    productList.splice(x,1)
-    localStorage.setItem("productData", JSON.stringify(productList));    
+function deleteSite(x){
+    siteList.splice(x,1)
+    localStorage.setItem("siteList", JSON.stringify(siteList));    
     display();
 }
 function display(){
     var temp = "";
-    for(var i = 0;i<productList.length;i++){
+    for(var i = 0;i<siteList.length;i++){
         temp += `<tr>
         <td>${i}</td>
-        <td>${productList[i].name}</td>
+        <td>${siteList[i].name}</td>
       
-        <td> <button onclick = "window.open('${productList[i].url}', '_blank');" target = "_blank"  class="btn btn-warning"> Visit</button> </td>
-        <td> <button onclick ="deleteProduct(${i})" class="btn btn-danger"> Delete</button> </td>
+        <td> <button onclick = "window.open('${siteList[i].url}', '_blank');" target = "_blank"  class="btn btn-warning"> Visit</button> </td>
+        <td> <button onclick ="deleteSite(${i})" class="btn btn-danger"> Delete</button> </td>
     </tr> `
     
     }
     document.getElementById("myData").innerHTML = temp;
     
 }
-//"location.href = '${productList[i].url}'"
